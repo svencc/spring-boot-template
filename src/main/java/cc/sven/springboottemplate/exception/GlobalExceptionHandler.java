edf85008bf500a1da4b97725666b50486c13ba31
Sven Carrillo Castillo <Carrillocastillo@adesso-mobile.de>
2022-10-03 13:01:31 +0200

    add GlobalExceptionHandler for Validation Failures

--- src/main/java/cc/sven/springboottemplate/exception/GlobalExceptionHandler.java
+++ src/main/java/cc/sven/springboottemplate/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package cc.sven.springboottemplate.exception;
 
+import cc.sven.springboottemplate.dto.ValidationFailedDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
@@ -15,6 +19,16 @@ import java.util.List;
 @ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ValidationFailedDto.builder()
+                        .validatedObject(ex.getParameter().getParameterName())
+                        .message(ex.getMessage())
+                        .build()
+                );
+    }
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<List<Void>> handleException(@NonNull final Exception exception) {
